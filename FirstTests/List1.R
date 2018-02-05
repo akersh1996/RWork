9bce9054193314f511cd4524f911dd8dd9d812cc
akersh1996 <kersh.andy@gmail.com>
2018-02-05 22:31:26 +0000

    update

---
+++ FirstTests/List1.R
@@ -0,0 +1,10 @@
+lst1 <- list(1,"two",3,4,5,6,7)
+# Indexing into the list - selecting a subset
+
+# +ve indexes mean return items identified
+print("+ve index test")
+print(lst1[c(1,2,3)])
+
+# -ve indexes mean return everything apart from
+print("-ve index test")
+print(lst1[-c(1,2,3)])
