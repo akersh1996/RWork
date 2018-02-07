512479dac854da7f3577e1ae68cf9f1db969e431
akersh1996 <kersh.andy@gmail.com>
2018-02-07 19:29:31 +0000

    update

---
+++ FirstTests/ShinyWebApps/github1.R
@@ -0,0 +1,16 @@
+# Examples from
+# https://github.com/rstudio/shiny-examples
+
+#if (!require(devtools))
+#  install.packages("devtools")
+# devtools::install_github("rstudio/leaflet")
+# The above doesn't work on my ubuntu environment but the examples still appear to work
+
+runShinyExample<- function(x){
+  if (x==1) shiny::runGitHub("rstudio/shiny-examples", subdir="081-widgets-gallery")
+  # Next example doesnt work
+  if (x==2) shiny::runGitHub("rstudio/shiny-examples", subdir="117-shinythemes")
+  if (x==3) shiny::runGitHub("rstudio/shiny-examples", subdir="027-absolutely-positioned-panels")
+}
+
+runShinyExample(2)
